35b4ba263cb1e52d2171348aff0b9ddc753b1682
florian935 <florian.martin63000@gmail.com>
2022-02-03 23:36:06 +0100

    Initial commit

---
+++ src/main/java/com/florian935/mongodb/reactive/polymorphism/ReactivePolymorphismApplication.java
@@ -0,0 +1,13 @@
+package com.florian935.mongodb.reactive.polymorphism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivePolymorphismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivePolymorphismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florian935/mongodb/reactive/polymorphism/ReactivePolymorphismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.mongodb.reactive.polymorphism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivePolymorphismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
